7b90630fc9add14b6a1508b08c7522e4fc261c63
Legaspi111 <60307038+Legaspi111@users.noreply.github.com>
2021-11-01 05:48:18 -0700

    Add files via upload

---
+++ script/App.js
@@ -0,0 +1,156 @@
+// Code goes here
+
+var enterButton = document.getElementById("addTaskBtn");
+enterButton.addEventListener("click", addTaskAfterClick);
+var input = document.getElementById("taskInput");
+input.addEventListener("keypress", addTaskAfterKeypress);
+var ul = document.querySelector("ul");
+//var item = document.getElementsByTagName("li");
+
+function addTaskAfterClick() {
+  //Do not allow an empty task name
+  if (inputLength() > 0) {
+    createTaskPanel();
+  }
+}
+
+function addTaskAfterKeypress(event) {
+  //when task name is not empty and hit enter button
+  if (inputLength() > 0 && event.which === 13) {
+    createTaskPanel();
+  }
+}
+
+function inputLength() {
+  return input.value.length;
+}
+
+function createTaskPanel() {
+  var time = 0;
+  var running = 0;
+
+  var resetTimer = false;
+  //create a task element and add to ul
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  //clear text input field
+  input.value = "";
+  //add del button
+  var delBtn = document.createElement("button");
+  //delBtn.appendChild(document.createTextNode("X"));
+  delBtn.innerHTML = "<i class='fa fa-trash'></i>";
+
+  li.appendChild(delBtn);
+  delBtn.addEventListener("click", deleteListItem);
+
+  //add timer
+  var timerSpan = document.createElement("span");
+  timerSpan.setAttribute("id", "stopWatchDisplay");
+  timerSpan.classList.add("timerDisplay");
+  timerSpan.innerHTML = "00:00:00";
+  li.appendChild(timerSpan);
+
+  //add start button
+  var startBtn = document.createElement("button");
+  startBtn.innerHTML = "<span><i class='fa fa-play-circle'></i></span>";
+  startBtn.setAttribute("id", "startBtn");
+  li.appendChild(startBtn);
+  startBtn.addEventListener("click", startTimer);
+  //delete a task by setting display:none
+
+  //add pause button
+  var pauseBtn = document.createElement("button");
+  //delBtn.appendChild(document.createTextNode("X"));
+  pauseBtn.innerHTML = "<span><i class='fa fa-pause-circle'></i></span>";
+  pauseBtn.setAttribute("id", "pauseBtn");
+
+  li.appendChild(pauseBtn);
+  pauseBtn.addEventListener("click", pauseTimer);
+  //delete a task by setting display:none
+
+  //add stop button
+  var stopBtn = document.createElement("button");
+  stopBtn.innerHTML = "<span><i class='fa fa-check-circle'></i></span>";
+  stopBtn.setAttribute("id", "stopBtn");
+
+  li.appendChild(stopBtn);
+  stopBtn.addEventListener("click", stopTimer);
+
+  function pauseTimer() {
+    li.classList.add("paused");
+    li.classList.remove("started");
+    li.classList.remove("done");
+    running = 0;
+    startBtn.enabled = true;
+    pauseBtn.enabled = false;
+    stopBtn.enabled = true;
+    // console.log("pause:" + resetTimer);
+  }
+  function startTimer() {
+    //console.log("start enter:" + resetTimer);
+    //The classList property is not supported in Internet Explorer 9 and earlier versions.
+    //li.classList.toggle("started");
+    if (resetTimer) {
+      reset();
+    }
+
+    if (running == 0) {
+      running = 1;
+      increment(timerSpan);
+      startBtn.enabled = false;
+      pauseBtn.enabled = true;
+      stopBtn.enabled = true;
+    }
+
+    li.classList.add("started");
+    li.classList.remove("paused");
+    li.classList.remove("done");
+    // console.log("start exit:" + resetTimer);
+  }
+
+  function stopTimer() {
+    li.classList.add("done");
+    li.classList.remove("paused");
+    li.classList.remove("started");
+    running = 0;
+    startBtn.enabled = true;
+    pauseBtn.enabled = false;
+    stopBtn.enabled = false;
+    resetTimer = true;
+    // console.log("stop:" + resetTimer);
+  }
+  function reset() {
+    running = 0;
+    time = 0;
+    resetTimer = false;
+    timerSpan.innerHTML = "00:00:00";
+  }
+  function increment() {
+    if (running == 1) {
+      setTimeout(function() {
+        time++;
+        var mins = Math.floor(time / 10 / 60) % 60;
+        var secs = Math.floor(time / 10) % 60;
+        var tenths = time % 10;
+
+        if (mins < 10) {
+          mins = "0" + mins;
+        }
+        if (secs < 10) {
+          secs = "0" + secs;
+        }
+
+        timerSpan.innerHTML = mins + ":" + secs + ":" + "0" + tenths;
+        increment();
+      }, 100);
+    }
+  }
+
+  //delete a task by setting display:none
+  function deleteListItem() {
+    //The classList property is not supported in Internet Explorer 9 and earlier versions.
+    li.classList.add("delete");
+  }
+}
+ 
